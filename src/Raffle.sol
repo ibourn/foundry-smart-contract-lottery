dc7e3a98b31ca0e03a4afd8b7cccfd35b585f310
ibourn <igor.bournazel@orange.fr>
2024-02-10 19:34:06 +0100

    :construction: intro custom error & event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,27 @@
 
 pragma solidity ^0.8.18;
 
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 /**
  * @title A sample Raffle contract
  * @author ibourn
@@ -9,7 +30,32 @@ pragma solidity ^0.8.18;
  * @dev implements Chainlink VRFv2
  */
 contract Raffle {
-    function enterRaffle() public {}
+    /** Errors */
+    error Raffle__NotEnoughEthSent();
+
+    /** State variables */
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+    }
 
     function pickWinner() public {}
+
+    /****************************************************************************
+     * Getter functions
+     ****************************************************************************/
+
+    /** Getter funcitons */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
